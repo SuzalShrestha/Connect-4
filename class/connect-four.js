b5f2ea315b7acce9ff1a25696033bb6a8a36f74b
sujal_shrestha <sujal.scxi121@amv.edu.np>
2023-03-02 15:45:26 +0545

    Completed

--- class/connect-four.js
+++ class/connect-four.js
@@ -38,7 +38,75 @@ class ConnectFour {
     // Return 'O' if player O wins
     // Return 'T' if the game is a tie
     // Return false if the game has not ended
+    let masterGrid=this.getmasterGrid(grid);
+   
+      
+    
 
+  }
+  static getmasterGrid(grid){
+    let masterGrid=[];
+    for(let item of grid){
+        masterGrid.push(item);
+    }
+    for(let i=0;i<grid.length+1;i++){
+      let verticalGrid=[];
+      for(let j=0;j<grid.length;j++){
+        verticalGrid.push(grid[j][i]);
+      }
+      masterGrid.push(verticalGrid);
+      //diagonal
+      
+
+    }
+    let diagonal=[]; //select a top left 3X4 portion of grid
+      for(let k=0;k<3;k++){
+        // let diagonal1=[];
+        for(l=0;l<4;l++){
+        //   diagonal1.push(grid[k][l]);
+          let diagmini=[grid[k][l]];
+          let row=k;
+          let col=l;
+
+            for(p=0;p<3;p++){
+               
+                
+                diagmini.push(grid[row+=1][col+=1]);
+                
+            }
+           diagonal.push(diagmini);
+        }
+      }
+      masterGrid.push(...diagonal);
+      //reverse diagonal
+      let reverseGrid=[];
+      for(let item of grid){
+        reverseGrid.push(item.reverse());
+      }
+      diagonal=[]; //select a top left 3X4 portion of grid
+      for(let k=0;k<3;k++){
+        // let diagonal1=[];
+        for(l=0;l<4;l++){
+        //   diagonal1.push(grid[k][l]);
+          let diagmini=[grid[k][l]];
+          let row=k;
+          let col=l;
+
+            for(p=0;p<3;p++){
+               
+                
+                diagmini.push(grid[row+=1][col+=1]);
+                
+            }
+           diagonal.push(diagmini);
+        }
+      }
+      masterGrid.push(...diagonal);
+      
+    return masterGrid;
+      
+
+    
   }
 
   static endGame(winner) {
--- class/cursor.js
+++ class/cursor.js
@@ -21,12 +21,39 @@ class Cursor {
   setBackgroundColor() {
     Screen.setBackgroundColor(this.row, this.col, this.cursorColor);
   }
+  up(){
+    this.resetBackgroundColor();
+    if(this.row>0){
+      this.row+=-1;
+    }
+    this.setBackgroundColor();
+  }
+  down(){
+    
+    this.resetBackgroundColor();
+    if(this.row<2){
+      this.row+=1;
+    }
+    this.setBackgroundColor();
+  }
 
   left() {
     // Move cursor left
+    this.resetBackgroundColor();
+    if(this.col>0){
+      this.col+=-1;
+    }
+
+    this.setBackgroundColor();
+
   }
 
   right() {
+    this.resetBackgroundColor();
+    if(this.col<2){
+      this.col+=1;
+    }
+    this.setBackgroundColor();
     // Move cursor right
   }
 
